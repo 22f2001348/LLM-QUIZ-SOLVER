1e0113506349b5f9cb49365dc46149c6f9b76406
22f2001348 <22f2001348@ds.study.iitm.ac.in>
2025-11-29 13:29:41 +0530

    Update shared_store.py

--- shared_store.py
+++ shared_store.py
@@ -1,2 +1,27 @@
-BASE64_STORE = {}
-url_time = {}
+# shared_store.py
+import threading
+from typing import Dict, Any
+
+class ThreadSafeDict:
+    def __init__(self):
+        self._data = {}
+        self._lock = threading.Lock()
+    
+    def get(self, key, default=None):
+        with self._lock:
+            return self._data.get(key, default)
+    
+    def __getitem__(self, key):
+        with self._lock:
+            return self._data[key]
+    
+    def __setitem__(self, key, value):
+        with self._lock:
+            self._data[key] = value
+    
+    def clear(self):
+        with self._lock:
+            self._data.clear()
+
+url_time = ThreadSafeDict()
+BASE64_STORE = ThreadSafeDict()
